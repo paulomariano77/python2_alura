d6f3360be862182bf03ac9a72e79b3b8cc26ddf6
Paulo Mariano <paulomariano77@gmail.com>
2017-07-26 13:43:18 -0300

    Exercises of class 06

---
+++ app.py
@@ -0,0 +1,66 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Lesson 6 - Organizing our code better
+# ----------------------------------------------------------------------------------------------------------------------
+# -*- coding: UTF-8 -*-
+
+from sys import stdout
+
+
+def register(names):
+    name = raw_input('\nInput your name: ')
+    names.append(name)
+
+
+def to_list(names):
+    print '\nListing names: '
+    for name in names:
+        print name
+
+
+def remove_names(names):
+    name = raw_input('\nWhat name would you like to remove?\n')
+    names.remove(name)
+    return names
+
+
+def update_names(names):
+    name = raw_input('\nWhat name would you like to edit?')
+    if (search(name, names)):
+        index = names.index(name)
+        new_name = raw_input('Type it a new name: ')
+        names[index] = new_name
+
+
+def search(name, names):
+    if (name in names):
+        print 'Name %s finded' % (name)
+        return True
+    else:
+        print "Name %s don't finded" % (name)
+
+def menu():
+    names = []
+    option = 0
+    while (option != 4):
+        print '\nType it: 0 to register, 1 to list, 2 to remove, 3 to update name or 4 to exit'
+        option = int(raw_input())
+
+        if (option == 0):
+            register(names)
+
+        if (option == 1):
+            to_list(names)
+
+        if (option == 2):
+            if (len(remove_names(names)) > 0):
+                print 'Your new name is: '
+                for name in names:
+                    stdout.write(name + " ")
+            else:
+                print 'No search elements'
+
+        if (option == 3):
+            update_names(names)
+
+
+menu()
